bb2cc2e9e6610fcb1c10f190d8aa44f80f146549
caioriegert <caioriegert@gmail.com>
2024-07-14 21:50:26 -0300

    Mp3 funcionando - Alterações: 1)Deixei funcional o upload de músicas 2)Adicionei a biblioteca jsmediatags para extrair os metadados das músicas

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const playPauseSongButton = document.getElementById('play-pause-song-button');
 const nextSongButton = document.getElementById('next-song-button');
 const previousSongButton = document.getElementById('previous-song-button');
@@ -5,6 +6,88 @@ const uploadSongButton = document.getElementById('upload-button');
 const shuffleButton = document.getElementById('shuffle-button');
 const timeBar = document.getElementById('time-bar');
 const timeBarDisplay = document.getElementById('time-bar-display');
+
+uploadSongButton.addEventListener('change', async () => {
+    const files = uploadSongButton.files;
+    
+    arrBlobAudio = [...files];
+    arrTempReaderAudio = [...files]
+
+    console.log(arrBlobAudio)
+
+    await turnIntoData();
+    sendToPlayList();
+})
+
+
+
+const turnIntoData = () => {
+    return Promise.all(
+        arrTempReaderAudio.map((file) => {
+            return new Promise((resolve) => {
+                if (file instanceof Blob) {
+                    const reader = new FileReader();
+                    reader.readAsDataURL(file);
+                    reader.addEventListener('load', (e) => {
+                        const readerTarget = e.target;
+                        arrFileReader.push(readerTarget.result);
+                        resolve();
+                    });
+                }
+            });
+        })
+    );
+};
+
+const sendToPlayList = () => {
+    const dataArray = arrBlobAudio.map((info, index) => ({
+        info: info,
+        data: arrFileReader[index]
+    }));
+    
+    dataArray.forEach((e) => {
+        jsmediatags.read(e.info, {
+                onSuccess: function(tag) {
+                    
+                    console.log(tag)
+
+                    try{
+                        var data = tag.tags.picture.data;
+                        var format = tag.tags.picture.format
+                        let base64String = "";
+                        for (const i = 0; i < data.length; i++) {
+                        base64String += String.fromCharCode(data[i]);
+                        }
+
+                        var imageUrl = `url(data:${format};base64,${window.btoa(base64String)})`
+                    }catch{
+                        console.log('no album image')
+                    }
+                    console.log("s")
+                    playListSongs.push(
+                        {
+                            songName: tag.tags.title || "Desconhecido",
+                            songArtist: tag.tags.artist || null,
+                            songAlbum: tag.tags.album || null,
+                            songFile: e.data,
+                            albumPicture: imageUrl || null,
+                        }
+                    )
+                    console.log(playListSongs);
+                    currentPlayList = [...playListSongs];
+                    showPlaylistSongs();
+                },
+                onError: function(error) {
+                    return console.log(error)
+                }
+        });
+    })
+}
+
+const jsmediatags = window.jsmediatags
+let arrBlobAudio = []
+let arrTempReaderAudio = []
+let arrFileReader = []
 
 let audio = new Audio();
 
@@ -26,67 +109,11 @@ let playListSongs = [
         songArtist: "Amy Winehouse",
         songAlbum: "Back to Black",
         songFile: "../songs/some-unholy-war.mp3",
-    },
-    {
-        songName: "Some Unholy War",
-        songArtist: "Amy Winehouse",
-        songAlbum: "Back to Black",
-        songFile: "../songs/some-unholy-war.mp3",
-    },
-    {
-        songName: "Some Unholy War",
-        songArtist: "Amy Winehouse",
-        songAlbum: "Back to Black",
-        songFile: "../songs/some-unholy-war.mp3",
-    },
-    {
-        songName: "Some Unholy War",
-        songArtist: "Amy Winehouse",
-        songAlbum: "Back to Black",
-        songFile: "../songs/some-unholy-war.mp3",
-    },
-    {
-        songName: "Some Unholy War",
-        songArtist: "Amy Winehouse",
-        songAlbum: "Back to Black",
-        songFile: "../songs/some-unholy-war.mp3",
-    },
-    {
-        songName: "Some Unholy War",
-        songArtist: "Amy Winehouse",
-        songAlbum: "Back to Black",
-        songFile: "../songs/some-unholy-war.mp3",
-    },
-    {
-        songName: "Some Unholy War",
-        songArtist: "Amy Winehouse",
-        songAlbum: "Back to Black",
-        songFile: "../songs/some-unholy-war.mp3",
-    },
-    {
-        songName: "Some Unholy War",
-        songArtist: "Amy Winehouse",
-        songAlbum: "Back to Black",
-        songFile: "../songs/some-unholy-war.mp3",
-    },
-    {
-        songName: "Some Unholy War",
-        songArtist: "Amy Winehouse",
-        songAlbum: "Back to Black",
-        songFile: "../songs/some-unholy-war.mp3",
-    },
-    {
-        songName: "Some Unholy War",
-        songArtist: "Amy Winehouse",
-        songAlbum: "Back to Black",
-        songFile: "../songs/some-unholy-war.mp3",
-    },
+    }
 ]
-
 
 let currentPlayList = [...playListSongs];
 let currentSongIndex = 0;
-
 
 const playSong = (indexPlaylist) => {
     const songToPlay = currentPlayList[indexPlaylist]?.songFile;
@@ -182,7 +209,7 @@ const showPlaylistSongs = () => {
                 <div class="song">
                         <div class="playlist-song-info" onclick="playChosenSong(${currentPlayList.indexOf(element)})">
                             <span class="song-name-playlist">${element?.songName}</span>
-                            <span class="song-artist-album-playlist">${element?.songArtist} - ${element?.songAlbum}</span>
+                            <span class="song-artist-album-playlist">${element?.songArtist? element?.songArtist :""}${element?.songAlbum? " - " + element?.songAlbum: ""}</span>
                         </div>
                         
                     </div>
